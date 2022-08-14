415b58330f477072682ed73c754da98dc92ce015
Paaritoosh <109455621+Paaritoosh@users.noreply.github.com>
2022-08-15 04:01:53 +0530

    Update Screen time.py

--- Screen time.py
+++ Screen time.py
@@ -5,14 +5,14 @@ import subprocess
 class Screen_time(rumps.App):
     def __init__(self):
         super(Screen_time, self).__init__("Screen Time")
-        self.menu = ["Usage","Clean Quit"]
+        self.menu = ["Usage","Quit","Clean Quit"]
         self._quit_button =None
 
     @rumps.clicked("Usage")
     def prefs(self,_):
         subprocess.run("open /System/Library/PreferencePanes/Screentime.prefPane",shell=True)
 
-    @rumps.clicked('Clean Quit')
+    @rumps.clicked('Quit')
     def clean_up_before_quit(self,_):
         To_close_System_prefrences = """osascript -e '
 
@@ -22,9 +22,8 @@ class Screen_time(rumps.App):
 
         '"""
         os.system(To_close_System_prefrences)
-        rumps.quit_application()
 
-    @rumps.clicked('Quit')
+    @rumps.clicked('Clean Quit')
     def quit_Screen_time(self,_):
         rumps.quit_application()
 
